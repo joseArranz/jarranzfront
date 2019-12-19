2ba4cd8d2c0cd6000e460c29875818db59d57c40
DESKTOP-N62HNGM\Jose <jose.arranz.becerril.jab@gmail.com>
2019-12-19 11:20:10 +0100

    revert production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  API_ENDPOINT_USER: "http://localhost:8081/user/",
-  API_ENDPOINT_OAUTH: "http://localhost:8081/oauth/token?grant_type=password&username=bill&password=abc123"
+  API_ENDPOINT_USER: "https://jarranz.herokuapp.com/user/",
+  API_ENDPOINT_OAUTH: "https://jarranz.herokuapp.com/oauth/token?grant_type=password&username=bill&password=abc123"
 };
 
 /*
